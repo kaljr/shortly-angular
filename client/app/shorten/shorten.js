52cde2e1377f43c16d306e69c33e30ed7b785348
kaljr <kenny.langston@gmail.com>
2015-04-23 17:29:55 -0700

    filtering woo!

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,10 +1,10 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
+.controller('ShortenController', function ($scope, $location, Links, $injector) {
   // Your code here
   $scope.link = { url : ''};
   $scope.newLink = '';
-
+  console.log($injector);
   $scope.addLink = function() {
     if($scope.newLink && $scope.newLink.length) {
       $scope.link.url = $scope.newLink;
